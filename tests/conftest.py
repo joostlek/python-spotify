a5ccbe294bcd9b07b4450e165eeae6c975bb7c6a
Joost Lekkerkerker <joostlek@outlook.com>
2023-12-18 00:22:59 +0100

    Add test fixture for spotify client (#30)
    * Add test fixture for spotify client
    
    * Add test fixture for spotify client

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
 """Asynchronous Python client for Spotify."""
+from typing import AsyncGenerator
+
+import aiohttp
 import pytest
 
+from spotifyaio import SpotifyClient
 from syrupy import SnapshotAssertion
 
 from .syrupy import SpotifySnapshotExtension
@@ -10,3 +14,21 @@ from .syrupy import SpotifySnapshotExtension
 def snapshot_assertion(snapshot: SnapshotAssertion) -> SnapshotAssertion:
     """Return snapshot assertion fixture with the Spotify extension."""
     return snapshot.use_extension(SpotifySnapshotExtension)
+
+
+@pytest.fixture(name="spotify_client")
+async def client() -> AsyncGenerator[SpotifyClient, None]:
+    """Return a Spotify client."""
+    async with aiohttp.ClientSession() as session, SpotifyClient(
+        session=session,
+    ) as spotify_client:
+        yield spotify_client
+
+
+@pytest.fixture(name="authenticated_client")
+async def authenticated_client(
+    spotify_client: SpotifyClient,
+) -> SpotifyClient:
+    """Return an authenticated Spotify client."""
+    spotify_client.authenticate("test")
+    return spotify_client
