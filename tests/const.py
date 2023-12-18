64b14bd8de552e3f5eabe5b49cace770d4e72b42
Joost Lekkerkerker <joostlek@outlook.com>
2023-12-18 20:49:37 +0100

    Bump version to 0.2.0 (#40)
    * Add method to fetch users
    
    * Bump version to 0.2.0
    
    * Bump version to 0.2.0

--- tests/const.py
+++ tests/const.py
@@ -1,9 +1,12 @@
 """Constants for tests."""
+from importlib import metadata
 
 SPOTIFY_URL = "https://api.spotify.com:443"
 
+version = metadata.version("spotifyaio")
+
 HEADERS = {
-    "User-Agent": "AioSpotify/0.0.1",
+    "User-Agent": f"AioSpotify/{version}",
     "Accept": "application/json, text/plain, */*",
     "Authorization": "Bearer test",
 }
