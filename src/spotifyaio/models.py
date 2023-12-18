bc21d8c06a6ceade5fa1e49a01146457fe8c0efe
Joost Lekkerkerker <joostlek@outlook.com>
2023-12-18 20:21:56 +0100

    Add methods to fetch playlists (#38)

--- src/spotifyaio/models.py
+++ src/spotifyaio/models.py
@@ -77,8 +77,8 @@ class AlbumType(StrEnum):
 class Image(DataClassORJSONMixin):
     """Image model."""
 
-    height: int
-    width: int
+    height: int | None
+    width: int | None
     url: str
 
 
@@ -160,3 +160,70 @@ class PlaybackState(CurrentPlaying):
     device: Device
     shuffle: bool = field(metadata=field_options(alias="shuffle_state"))
     repeat_mode: RepeatMode = field(metadata=field_options(alias="repeat_state"))
+
+
+class PlaylistOwnerType(StrEnum):
+    """Playlist owner type."""
+
+    USER = "user"
+
+
+@dataclass
+class PlaylistOwner(DataClassORJSONMixin):
+    """Playlist owner model."""
+
+    display_name: str
+    external_urls: dict[str, str]
+    href: str
+    owner_id: str = field(metadata=field_options(alias="id"))
+    object_type: str = field(metadata=field_options(alias="type"))
+    uri: str
+
+
+@dataclass
+class BasePlaylist(DataClassORJSONMixin):
+    """Base playlist model."""
+
+    collaborative: bool
+    description: str | None
+    external_urls: dict[str, str]
+    playlist_id: str = field(metadata=field_options(alias="id"))
+    images: list[Image]
+    name: str
+    owner: PlaylistOwner
+    public: bool | None
+    object_type: str = field(metadata=field_options(alias="type"))
+    uri: str
+
+
+@dataclass
+class Playlist(BasePlaylist):
+    """Playlist model."""
+
+
+@dataclass
+class PlaylistResponse(DataClassORJSONMixin):
+    """Playlist response model."""
+
+    href: str
+    items: list[BasePlaylist]
+    limit: int
+    next_list: str | None = field(metadata=field_options(alias="next"))
+    offset: int
+    previous_list: str | None = field(metadata=field_options(alias="previous"))
+    total: int
+
+
+@dataclass
+class FeaturedPlaylistResponse(DataClassORJSONMixin):
+    """Featured playlist response model."""
+
+    message: str
+    playlists: PlaylistResponse
+
+
+@dataclass
+class CategoryPlaylistResponse(DataClassORJSONMixin):
+    """Category playlist response model."""
+
+    playlists: PlaylistResponse
---
+++ tests/test_playlist.py
@@ -0,0 +1,102 @@
+"""Asynchronous Python client for Spotify."""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from aiohttp.hdrs import METH_GET
+from aioresponses import aioresponses
+
+from . import load_fixture
+from .const import HEADERS, SPOTIFY_URL
+
+if TYPE_CHECKING:
+    from spotifyaio.spotify import SpotifyClient
+    from syrupy import SnapshotAssertion
+
+
+async def test_get_playlist(
+    responses: aioresponses,
+    snapshot: SnapshotAssertion,
+    authenticated_client: SpotifyClient,
+) -> None:
+    """Test retrieving playlist."""
+    responses.get(
+        f"{SPOTIFY_URL}/v1/playlists/37i9dQZF1DXcBWIGoYBM5M",
+        status=200,
+        body=load_fixture("playlist.json"),
+    )
+    response = await authenticated_client.get_playlist("37i9dQZF1DXcBWIGoYBM5M")
+    assert response == snapshot
+    responses.assert_called_once_with(
+        f"{SPOTIFY_URL}/v1/playlists/37i9dQZF1DXcBWIGoYBM5M",
+        METH_GET,
+        headers=HEADERS,
+        params=None,
+        data=None,
+    )
+
+
+async def test_get_current_users_playlists(
+    responses: aioresponses,
+    snapshot: SnapshotAssertion,
+    authenticated_client: SpotifyClient,
+) -> None:
+    """Test retrieving playback state."""
+    responses.get(
+        f"{SPOTIFY_URL}/v1/me/playlists?limit=48",
+        status=200,
+        body=load_fixture("current_user_playlist.json"),
+    )
+    response = await authenticated_client.get_playlists_for_current_user()
+    assert response == snapshot
+    responses.assert_called_once_with(
+        f"{SPOTIFY_URL}/v1/me/playlists",
+        METH_GET,
+        headers=HEADERS,
+        params={"limit": 48},
+        data=None,
+    )
+
+
+async def test_get_featured_playlists(
+    responses: aioresponses,
+    snapshot: SnapshotAssertion,
+    authenticated_client: SpotifyClient,
+) -> None:
+    """Test retrieving playback state."""
+    responses.get(
+        f"{SPOTIFY_URL}/v1/browse/featured-playlists?limit=48",
+        status=200,
+        body=load_fixture("featured_playlists.json"),
+    )
+    response = await authenticated_client.get_featured_playlists()
+    assert response == snapshot
+    responses.assert_called_once_with(
+        f"{SPOTIFY_URL}/v1/browse/featured-playlists",
+        METH_GET,
+        headers=HEADERS,
+        params={"limit": 48},
+        data=None,
+    )
+
+
+async def test_get_category_playlists(
+    responses: aioresponses,
+    snapshot: SnapshotAssertion,
+    authenticated_client: SpotifyClient,
+) -> None:
+    """Test retrieving playback state."""
+    responses.get(
+        f"{SPOTIFY_URL}/v1/browse/categories/rock/playlists?limit=48",
+        status=200,
+        body=load_fixture("category_playlists.json"),
+    )
+    response = await authenticated_client.get_category_playlists("rock")
+    assert response == snapshot
+    responses.assert_called_once_with(
+        f"{SPOTIFY_URL}/v1/browse/categories/rock/playlists",
+        METH_GET,
+        headers=HEADERS,
+        params={"limit": 48},
+        data=None,
+    )
